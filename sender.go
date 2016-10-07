1ebd3713e3ec97b0ada16e74c8926c0d15a40d29
fegan104 <fegan104@gmail.com>
2016-10-06 22:50:26 -0400

    we now have emoji

--- sender.go
+++ sender.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+
+    "github.com/kyokomi/emoji"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+    fmt.Println("Hello Wolrd Emoji!")
+
+    emoji.Println(":beer: Beer!!!")
+
+    pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
+    fmt.Println(pizzaMessage)
 }
